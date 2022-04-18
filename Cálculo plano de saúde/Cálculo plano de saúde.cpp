02f41ebb04e45ddba445d05218d3c40cbf8f3719
Ruan Pablo <100460553+RuanPablo1@users.noreply.github.com>
2022-04-18 18:18:21 -0300

    Cálculo plano de saúde

---
+++ Cálculo plano de saúde/Cálculo plano de saúde.cpp
@@ -0,0 +1,47 @@
+/******************************************************************************
+
+Cálculo do desconto do plano de saúde
+
+*******************************************************************************/
+#include <cstdio>
+#include <cmath>
+#include <cstring>
+#include <cstdlib>
+
+int main()
+{
+
+float salario, descontomenor, descontomaior, liquido;
+int dependentesmenor,dependentesmaior;
+	
+	printf("Digite aqui o seu salario: ");
+	scanf("%f",&salario);
+	
+	
+	printf("\nDigite a quantidade de dependentes menores de 18 anos que voce tem: ");
+	scanf("%d",&dependentesmenor);
+	
+	
+	printf("\nDigite a quantidade de dependentes entre 18 e 24 anos que voce tem: ");
+	scanf("%d",&dependentesmaior);
+	
+	//Desconta os 3% do salário do funcionário	
+	liquido = salario - (salario * 0.03);
+			
+	//Desconta o valor dos dependentes até 17 anos
+	descontomenor = (salario*0.015)*dependentesmenor;		
+	printf("\nO valor de desconto com seus dependentes menores fica %0.2f \n",descontomenor);
+			
+	//Desconta o valor dos dependentes entre 18 e 24 anos
+	descontomaior = (salario*0.025)*dependentesmaior;			
+	printf("\nO valor de desconto com seus dependentes maiores fica %0.2f \n",descontomaior);
+	
+	//Dá o salário líquido final
+	liquido = (liquido - descontomenor - descontomaior);
+		
+	printf("\nO valor do seu salario com desconto e: %0.2f \n", liquido);
+		
+	
+	return 0;
+}
+
