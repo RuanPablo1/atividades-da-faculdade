18b1f52d865acc76f124a549b45dfdde40b40b29
Ruan Pablo <100460553+RuanPablo1@users.noreply.github.com>
2022-04-18 18:16:26 -0300

    Exercícios - Matrizes

---
+++ Matrizes - Número par é 0/Número par é 0.cpp
@@ -0,0 +1,38 @@
+/******************************************************************************
+
+Número par é 0
+
+*******************************************************************************/
+#include <cstdio>
+#include <cmath>
+#include <cstring>
+#include <cstdlib>
+
+int main()
+{
+
+int matriz[5][5], L, C;
+
+printf("Digite os valores da matriz: \n");
+
+//Lê a matriz
+for(L = 0; L < 5; L++){
+	for(C = 0; C < 5; C++){
+		scanf("%d", &matriz[L][C]);
+		
+	}
+}
+
+//Transforma os pares em 0
+for(L=0; L < 5; L++){
+    for(C = 0; C <5; C++){
+        if(matriz[L][C]%2 == 0){
+        matriz[L][C] = 0;
+        }
+        printf("%2d|", matriz[L][C]);
+    }
+    printf("\n");
+}
+
+return 0;
+}
